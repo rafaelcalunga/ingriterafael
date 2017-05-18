c85f63e79347447d2c4a59496042bc1ac879a2ab
Rafael Calunga <rafaelcalunga@gmail.com>
2017-05-18 14:29:03 -0400

    Improvements in responsive layout.

--- js/site.js
+++ js/site.js
@@ -1,7 +1,5 @@
-$(function(){
-	$("#countdown").countdown("2017/07/15", function(event) {
-		$(this).text(
-			event.strftime('%D dias %H:%M:%S')
-		);
+$(function() {
+	$('#countdown').countdown('2017/07/15 17:00:00', function(event) {
+		$(this).text(event.strftime('%D dias %H:%M:%S') );
 	});
 })
